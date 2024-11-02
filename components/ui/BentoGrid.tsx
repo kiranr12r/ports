8aac2e6663ba1d53e1e93cf170b64999ac975c45
kiran12 <rkiru04@gmail.com>
2024-11-02 11:19:37 +0530

    added components

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -68,7 +68,7 @@ export const BentoGridItem = ({
     };
 
     const handleCopy = () => {
-        const text = "hsu@jsmastery.pro";
+        const text = "rkiru04@gmail.com";
         navigator.clipboard.writeText(text);
         setCopied(true);
     };
